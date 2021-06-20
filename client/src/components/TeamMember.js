334d7a044561442e4e772a05add8a2747785da8f
Sultan <sultan.singh.atwal@gmail.com>
2021-06-20 12:10:26 -0700

    #33 feat: add profile icons

--- client/src/components/TeamMember.js
+++ client/src/components/TeamMember.js
@@ -1,3 +1,5 @@
+import {FaLinkedin, FaGithub, FaStackOverflow} from 'react-icons/fa';
+
 const TeamMember = ({member}) => {
     return (
         <div className="team-member">
@@ -5,8 +7,16 @@ const TeamMember = ({member}) => {
                 <img src="https://picsum.photos/150" alt={member.Name} className="team-member-image" />
             </div>
 
-            <p className="team-member-name">{member.Name}</p>
-            <p className="team-member-role">{member.Role}</p>
+            <div className="member-name-role">
+                <p className="team-member-name">{member.Name}</p>
+                <p className="team-member-role">{member.Role}</p>
+            </div>
+
+            <div className="member-social-profile">
+                <a href={member.Linkedin}><FaLinkedin></FaLinkedin></a>
+                {member.Github !== "" && <a href={member.Github}><FaGithub></FaGithub></a>}
+                {member.Stackoverflow !== "" && <a href={member.Stackoverflow}><FaStackOverflow></FaStackOverflow></a>}
+            </div>
         </div>
     )
 }
