b3d39ee42bac5d6a89813eae39ee19d1e525bcf9
Sultan <sultan.singh.atwal@gmail.com>
2021-06-30 15:08:02 -0700

    #53 fix: market share viz on resize

---
+++ client/src/components/MarketShare/MarketShareFilter.js
@@ -0,0 +1,14 @@
+const MarketShareFilter = ({ setYear, yearData, order }) => {
+    return (
+        <select onChange={(e) => setYear(parseInt(e.target.value))}>
+            {
+                Object.keys(yearData).sort((a, b) => {
+                    if(order === "ascending") return a - b;
+                    else return b - a;
+                }).map(year => <option value={year}>{year}</option>)
+            }
+        </select>
+    )
+}
+
+export default MarketShareFilter
