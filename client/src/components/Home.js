86e4c097314086b74ec65f0fac74ecbd4941c13e
Sultan <sultan.singh.atwal@gmail.com>
2021-07-03 08:14:57 -0700

    #61 feat: remove diesel and gasoline from sales figures

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,33 +11,25 @@ import SalesFigures from "./SalesFigures/SalesFigures";
 
 const Home = () => {
 	const [salesFigures, setSalesFigures] = useState([]);
-	const [incentives, setIncentives] = useState([]);
 
 	useEffect(() => {
 		// Initially when the component renders the unmounted variable set to false.
 		let unmounted = false;
-		const getIncentives = async () => {
-			const url = "/api/incentives";
-			const res = await fetch(url);
-			const data = await res.json();
-			//   set the state only when the component is mounted
-			if (!unmounted) {
-				setIncentives(data);
-			}
-		};
 		
 		const getSalesFigures = async () => {
-			const url = "/api/sales";
-			const res = await fetch(url);
-			const data = await res.json();
+
+			// Set the state only when the component is mounted
 			if (!unmounted) {
-				// set the state only when the component is mounted
+				const url = "/api/sales";
+				const res = await fetch(url);
+				const data = await res.json();
+				
 				setSalesFigures(data);
 			}
 		};
 
 		getSalesFigures();
-		getIncentives();
+
 		// After this component gets unmounted unsubscribe any background calls.
 		return () => {
 			unmounted = true;
@@ -48,14 +40,14 @@ const Home = () => {
 		<div>
 			<HomeBanner></HomeBanner>
 			<div className="page-content">
-				<ElectricVehicleIntro></ElectricVehicleIntro>
-				<TypesOfEvs></TypesOfEvs>
-				<SalesFigures salesFigures={salesFigures}></SalesFigures>
-				<MarketShare salesFigures={salesFigures}></MarketShare>
-				<EvIncentives></EvIncentives>
-				<FindAnEv></FindAnEv>
-				<EvTimeline></EvTimeline>
-				<ReadEvNews></ReadEvNews>
+				<ElectricVehicleIntro />
+				<TypesOfEvs />
+				<SalesFigures salesFigures={salesFigures.filter(figure => (figure["Fuel type"] === "Battery electric" || figure["Fuel type"] === "Hybrid electric" || figure["Fuel type"] === "Plug-in hybrid electric") && figure["Vehicle type"] === "Total, vehicle type")} />
+				<MarketShare salesFigures={salesFigures} />
+				<EvIncentives />
+				<FindAnEv />
+				<EvTimeline />
+				<ReadEvNews />
 			</div>
 		</div>
 	);
