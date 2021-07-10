0912c376e2faa180213c5141004689ae010a847a
Sultan <sultan.singh.atwal@gmail.com>
2021-07-10 08:04:06 -0700

    #81 fix: contact form submission

--- client/src/components/ElectricVehicleIntro.js
+++ client/src/components/ElectricVehicleIntro.js
@@ -3,7 +3,7 @@ const ElectricVehicleIntro = () => {
         <section id="evIntro">
             <h2>What is meant by an electric vehicle?</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, delectus aperiam. Minus, deserunt maxime nesciunt est debitis ea temporibus quaerat iusto? Ipsam nam, fugiat doloremque expedita dolores architecto temporibus doloribus esse cumque dolorem accusantium in fugit porro iusto, nemo magni possimus illo, eum libero dolorum unde ad necessitatibus totam recusandae? Necessitatibus, incidunt perspiciatis nobis accusantium nisi expedita commodi aliquam eveniet excepturi earum minima ut dolor rerum alias cumque totam tempora ex.
+                An electric vehicle (EV) is a vehicle that uses one or more electric motors or traction motors for propulsion. An electric vehicle may be powered through a collector system by electricity from off-vehicle sources, or may be self-contained with a battery, solar panels, fuel cells or an electric generator to convert fuel to electricity.
             </p>
         </section>
     )
--- server.js
+++ server.js
@@ -41,11 +41,10 @@ app.use("/about", contactForm);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static('./client/build'));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(((__dirname = "client"), "build", "index.html")));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-  //  if block ends
 }
 
 const port = process.env.PORT || 5000;
