6f9e4d871b78fffa4224ed5c391a44266213d723
Sultan <sultan.singh.atwal@gmail.com>
2021-07-15 14:19:45 -0700

    #97 feat: add legend to market share viz

--- client/src/components/MarketShare/MarketShare.js
+++ client/src/components/MarketShare/MarketShare.js
@@ -3,7 +3,7 @@ import MarketShareViz from "./MarketShareViz";
 const MarketShare = ({salesFigures}) => {
     return (
         <section id="evMarket">
-            <h2>Canadian EV Market</h2>
+            <h2>Canadian EV Market Share</h2>
 
             <div>
                 <MarketShareViz salesFigures={salesFigures} order="ascending"></MarketShareViz>
