49764b860ea82a37412f2d5671fc0812bc414fcf
Sultan <sultan.singh.atwal@gmail.com>
2021-07-21 18:51:35 -0700

    #122 feat: responsive tools page

--- client/src/components/ToolsBanner1.js
+++ client/src/components/ToolsBanner1.js
@@ -1,15 +1,15 @@
-import Button from "./Button"
 import Illustration from '../static/images/illustration-calculate.png';
 
 const ToolsBanner1 = () => {
     return (
         <div className="tools-banner">
-            <img src={Illustration} alt="Comparison tool illustration" />
+            <img src={Illustration} alt="Comparison tool illustration" className="banner-illustration-large-screen" />
 
             <div>
                 <h2>Know the money you paid and the emissions your car produced</h2>
                 <p>Based on the data entered, we will help you calculate annual fuel cost and emission.</p>
-                <Button text="Let's figure it out"></Button>
+                <img src={Illustration} alt="Comparison tool illustration" className="banner-illustration-small-screen" />
+                <a href=""><button>Let's Figure It Out</button></a>
             </div>
         </div>
     )
