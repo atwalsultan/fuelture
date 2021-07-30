0ee42c2f85157286095516b8e2fa0a2fce2452a3
Sultan <sultan.singh.atwal@gmail.com>
2021-07-29 18:49:17 -0700

    #143 feat: add comparison infographic

--- client/src/components/CalculateCost.js
+++ client/src/components/CalculateCost.js
@@ -1,8 +1,14 @@
+import comparisonDesktop from '../static/images/comparison-desktop.jpg';
+import comparisonMobile from '../static/images/comparison-mobile.jpg';
+
 const CalculateCost = () => {
     return (
         <section id="calculateCost">
             <h2>Calculate your cost and emission</h2>
-            <div></div>
+            <div>
+                <img src={comparisonDesktop} alt="Comparison of EVs with a gas car" id="comparisonDesktop"/>
+                <img src={comparisonMobile} alt="Comparison of EVs with a gas car" id="comparisonMobile" />
+            </div>
         </section>
     )
 }
